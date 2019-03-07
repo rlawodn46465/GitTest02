c4e0e889670b7fc65033c288a647457e23ac1679
dit08 <dit@imac-3.local>
2019-03-07 17:09:55 +0900

    Initial Commit

---
+++ GitTest02/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTest02
+//
+//  Created by dit08 on 2019. 3. 7..
+//  Copyright © 2019년 dit08. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
