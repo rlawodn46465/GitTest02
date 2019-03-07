83c6bd6472b9784a4834bc7c2bcd1a8855e88f66
dit08 <dit@imac-3.local>
2019-03-07 17:41:28 +0900

    change color yellow

--- GitTest02/ViewController.swift
+++ GitTest02/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         // background color red
-        self.view.backgroundColor = UIColor.red
+        self.view.backgroundColor = UIColor.yellow
     }
 
 }
